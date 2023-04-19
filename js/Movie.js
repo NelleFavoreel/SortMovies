8bbe66acd09241a327287a3b109765c72197a7c4
NelleFavoreel <114076571+NelleFavoreel@users.noreply.github.com>
2023-04-19 11:00:30 +0200

    consoleInfo

---
+++ js/Movie.js
@@ -0,0 +1,10 @@
+export default class Movie {
+	constructor(crew, imDbRating, image, rank, title, year) {
+		this.crew = crew;
+		this.imDbRating = imDbRating;
+		this.image = image;
+		this.rank = rank;
+		this.title = title;
+		this.year = year;
+	}
+}
--- js/index.js
+++ js/index.js
@@ -1,9 +1,26 @@
 "use strict";
 
+import Movie from "./Movie.js";
+
 const app = {
-  init: function() {
-    console.log("hello world");
-  }
-}
+	init: function () {
+		this.getData();
+		console.log("hello world");
+	},
+	getData() {
+		fetch("https://imdb-api.com/en/API/Top250Movies/k_3blwlx4a")
+			.then((movieList) => movieList.json())
+			.then(function (response) {
+				response.items.forEach(function (movieInfo) {
+					const movie = new Movie(movieInfo.crew, movieInfo.imDbRating, movieInfo.image, movieInfo.rank, movieInfo.title, movieInfo.year);
+					console.log(movie);
+				});
+			});
+	},
+};
+var requestOptions = {
+	method: "GET",
+	redirect: "follow",
+};
 
 app.init();
