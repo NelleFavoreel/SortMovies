222a882567ad85c8c7a0cf57860d070164584de8
NelleFavoreel <114076571+NelleFavoreel@users.noreply.github.com>
2023-04-19 12:20:52 +0200

    showMovies

--- js/Movie.js
+++ js/Movie.js
@@ -7,4 +7,18 @@ export default class Movie {
 		this.title = title;
 		this.year = year;
 	}
+	get htmlString() {
+		return `
+        <article>
+          <img src=${this.image} alt="">
+          <div class="article_content_wrapper">
+            <div>
+              <h3>Name: ${this.title}</h3>
+              <h4>Year: ${this.year}</h4>
+			  <h4>Rank: ${this.rank}</h4>
+			  <h4>Rating: ${this.imDbRating}</h4>
+            </div>
+          </div>
+        </article>`;
+	}
 }
--- js/index.js
+++ js/index.js
@@ -13,10 +13,12 @@ const app = {
 			.then(function (response) {
 				response.items.forEach(function (movieInfo) {
 					const movie = new Movie(movieInfo.crew, movieInfo.imDbRating, movieInfo.image, movieInfo.rank, movieInfo.title, movieInfo.year);
+					document.querySelector("#content_section").insertAdjacentHTML("beforeend", movie.htmlString);
 					console.log(movie);
 				});
 			});
 	},
+	renderMovies() {},
 };
 var requestOptions = {
 	method: "GET",
