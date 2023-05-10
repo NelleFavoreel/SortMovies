13e7e3ea096cce4d0a8f6f390d43d777689ed3d9
NelleFavoreel <114076571+NelleFavoreel@users.noreply.github.com>
2023-05-10 11:45:28 +0200

    sort

--- js/index.js
+++ js/index.js
@@ -3,9 +3,23 @@
 import Movie from "./Movie.js";
 
 const app = {
+	movies: [],
+	userInput: {
+		selectedSort: "title",
+	},
+	applyAll() {
+		this.sortMovies();
+		this.renderMovies();
+	},
 	init: function () {
 		this.getData();
-		console.log("hello world");
+		const sortRadios = document.getElementsByName("sortBy");
+		sortRadios.forEach(function (radio) {
+			radio.addEventListener("change", function (event) {
+				app.userInput.selectedSort = this.value;
+				app.applyAll();
+			});
+		});
 	},
 	getData() {
 		fetch("https://imdb-api.com/en/API/Top250Movies/k_3blwlx4a")
@@ -13,12 +27,38 @@ const app = {
 			.then(function (response) {
 				response.items.forEach(function (movieInfo) {
 					const movie = new Movie(movieInfo.crew, movieInfo.imDbRating, movieInfo.image, movieInfo.rank, movieInfo.title, movieInfo.year);
+					app.movies.push(movie);
 					document.querySelector("#content_section").insertAdjacentHTML("beforeend", movie.htmlString);
-					console.log(movie);
 				});
 			});
+		console.log(app.userInput.selectedSort);
 	},
-	renderMovies() {},
+	sortMovies() {
+		app.movies.sort(function (a, b) {
+			let sortBy = app.userInput.selectedSort;
+
+			if (sortBy === "rank" || sortBy === "year") {
+				let numA = parseInt(a[sortBy]);
+				let numB = parseInt(b[sortBy]);
+				return numA - numB;
+			} else if (sortBy === "title") {
+				if (a[sortBy] < b[sortBy]) {
+					return -1;
+				} else if (a[sortBy] > b[sortBy]) {
+					return 1;
+				}
+			} else {
+				return 0;
+			}
+		});
+	},
+
+	renderMovies() {
+		document.querySelector("#content_section").innerHTML = "";
+		app.movies.forEach(function (movie) {
+			document.querySelector("#content_section").insertAdjacentHTML("beforeend", movie.htmlString);
+		});
+	},
 };
 var requestOptions = {
 	method: "GET",
